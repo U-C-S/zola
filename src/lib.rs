2e71dcfbe764a45307d9ea408f675d1bda29467b
U-C-S <uchanakyasrinivas@gmail.com>
2023-08-25 14:28:33 +0530

    working ports for `check`, `init` cmds

--- src/lib.rs
+++ src/lib.rs
@@ -5,19 +5,9 @@ mod prompt;
 
 use std::path::Path;
 
-use cmd::build;
+use cmd::{build, check, create_new_project, serve};
 use napi_derive::napi;
-
-// #[derive(Debug, Clone, Serialize)]
-// #[napi(object)]
-// pub struct BuildParams<'a> {
-//     pub root_dir: &'a Path,
-//     pub config_file: &'a Path,
-//     pub base_url: Option<&'a str>,
-//     pub output_dir: Option<&'a Path>,
-//     pub force: bool,
-//     pub include_drafts: bool,
-// }
+use time::UtcOffset;
 
 #[napi]
 pub fn zola_build(
@@ -37,3 +27,56 @@ pub fn zola_build(
         include_drafts,
     );
 }
+
+#[napi]
+pub fn zola_init(name: String, force: bool) {
+    let _ = create_new_project(name.as_str(), force);
+}
+
+#[napi]
+pub fn zola_check(
+    root_dir: String,
+    config_file: String,
+    base_path: Option<String>,
+    base_url: Option<String>,
+    include_drafts: bool,
+) {
+    let _ = check(
+        Path::new(&root_dir),
+        Path::new(&config_file),
+        base_path.as_ref().map(|t| t.as_str()),
+        base_url.as_deref(),
+        include_drafts,
+    );
+}
+
+/*
+#[napi]
+pub fn zola_serve(
+    root_dir: &Path,
+    interface: &str,
+    interface_port: u16,
+    output_dir: Option<&Path>,
+    base_url: &str,
+    config_file: &Path,
+    open: bool,
+    include_drafts: bool,
+    fast_rebuild: bool,
+    no_port_append: bool,
+    utc_offset: UtcOffset,
+) {
+    let _ = serve(
+        root_dir,
+        interface,
+        interface_port,
+        output_dir,
+        base_url,
+        config_file,
+        open,
+        include_drafts,
+        fast_rebuild,
+        no_port_append,
+        utc_offset,
+    );
+}
+*/
---
+++ test_site/build.mjs
@@ -0,0 +1,5 @@
+import { zolaBuild, zolaCheck, zolaInit } from "./index.js";
+
+// zolaBuild(".", "config.toml", "/",  "./public", false, false);
+// zolaCheck(".", "config.toml", "/", "/", false, false);
+zolaInit("abcd", false);
