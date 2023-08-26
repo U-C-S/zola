1cac801f0bacf5f315b84d0b1a8c6125d775ae68
U-C-S <uchanakyasrinivas@gmail.com>
2023-08-26 15:34:39 +0530

    working everything

--- test_site/build.mjs
+++ test_site/build.mjs
@@ -1,5 +1,7 @@
-import { zolaBuild, zolaCheck, zolaInit } from "./index.js";
+import { zolaBuild, zolaCheck, zolaInit, zolaServe } from "./index.js";
 
 // zolaBuild(".", "config.toml", "/",  "./public", false, false);
 // zolaCheck(".", "config.toml", "/", "/", false, false);
-zolaInit("abcd", false);
+// zolaInit("abcd", false);
+zolaServe("/home/greenpenguin/code/zola-npm/zola/test_site/", "127.0.0.1", 8000, null, "/", "config.toml", false, false, true, false);
+// zolaServe(".", "127.0.0.1", 8000, null, "/", "config.toml", false, false, true, false);
